fda1f766e884530d4f9da67f94db21078477ab70
pham-an-mulodo <pham.an@mulodo.com>
2013-10-11 17:51:06 +0700

    created login API. need add authentication in other API

--- AnPham/Notes/protected/controllers/AuthController.php
+++ AnPham/Notes/protected/controllers/AuthController.php
@@ -2,24 +2,23 @@
 class AuthController extends APIController {
 
 	public function actionLogin() {
-		$result = array(
-				'status' => 502,
-				'message' => ''
-		);
-		$identity = new ClientIdentity($username,$password);
+		$response = null;
+		$input_params = $this->getActionParams();
+		$email = $input_params['email'];
+		$password = $input_params['password'];
+		
+		$identity = new ClientIdentity($email,$password);
 		if($identity->authenticate()) {
 			Yii::app()->user->login($identity);
-			$result = array(
-					'status'=>200,
-					'message'=>'Login Success!'
+			$this->setHeader(200, 'Login Success!');
+			$response = array(
+					'token' => $identity->getState( 'token'),
+					'time' => $identity->getState( 'time'),
 			);
 		} else {
-			$result = array(
-					'status'=>200,
-					'messages'=> $identity->errorMessage,
-			);
+			$this->setHeader( 300, $identity->errorMessage);
 		}
-		$this->formatResult( $result);
+		$this->sendResponse( $response, 'token');
 	}
 
 	public function actionLogout() {

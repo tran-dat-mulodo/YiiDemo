ad4d3ab3f24080562b40d98c7f66eef6b6b4063a
Do Hung <do.hung@192.168.2.75>
2013-10-11 11:25:25 +0700

    RESTful source

--- HungDo/yii-blog-rest/index.php
+++ HungDo/yii-blog-rest/index.php
@@ -2,7 +2,7 @@
 
 // change the following paths if necessary
 // 4office
-$yii=dirname(__FILE__).'/../framework/yii.php';
+$yii=dirname(__FILE__).'/../../framework/yii.php';
 // 00584
 // $yii='C:/Frameworks/yii/yii.php';
 $config=dirname(__FILE__).'/protected/config/main.php';
